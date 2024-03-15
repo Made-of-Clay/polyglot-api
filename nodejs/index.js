7715b144cd36121c470ddc37decf1b88fedd101b
Adam Leis <aleis@iti4dmv.com>
2024-03-15 13:15:14 -0400

    decide on API endpoints & prep pug (some) for template use
    also tried establishing folder structure, but it's WIP

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,18 +1,39 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const pug = require('pug');
+// const foo = require('./foo');
+const app = express();
+const port = 3000;
+
+/*
+Home/List → /nodeapi[/index]
+Detail → /nodeapi/{countryId}
+*/
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Return list of countries');
 });
 
-app.get('/foo', (req, res) => {
-    res.send('bar');
+app.get('/index', (req, res) => {
+    res.redirect('/')
 });
+
+app.get('/{countryId}', (req, res) => {
+    // use cca3 for codes for countryId
+    res.send('Return list of countries');
+});
+
+// require('./foo');
+// console.log('foo module', foo)
+// foo();
+// app.get('/foo', (req, res) => {
+//     console.log(req.query)
+//     res.send('bar');
+// });
 
 app.get('/bar', (req, res) => {
     res.send('foo');
 });
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
